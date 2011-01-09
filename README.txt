2718e09345d0fc28cdf8aa4ba41bf735de9e575d
oliver <oliver.mueller@gmail.com>
2011-01-09 18:28:05 +0100

    added jquery file upload

---
+++ README.txt
@@ -0,0 +1,43 @@
+jQuery File Upload Plugin
+=========================
+
+Demo
+----
+http://aquantum-demo.appspot.com/file-upload
+
+Features
+--------
+  - Plugin for jQuery + jQuery UI (optional).
+  - Upload multiple files at once.
+  - Drag & Drop files from the desktop.
+  - Progress bar indicating the upload progress.
+  - Possibility to cancel uploads.
+  - No browser plugins (Adobe Flash, etc.) required.
+  - Standard HTML5 file upload form.
+  - Graceful iframe fallback for browsers not supporting XMLHttpRequest file uploads.
+  - Supports Cross-site XMLHttpRequests.
+  - Basic file upload works without JavaScript enabled.
+  - Files are sent as standard "multipart/form-data".
+  - Works with all Webhosting platforms including Google App Engine.
+
+Browser Support (tested versions)
+---------------------------------
+  - Google Chrome - 7.0, 8.0
+  - Apple Safari - 5.0
+  - Mozilla Firefox - 3.6
+  - Opera - 10.60 *, 11.00 *
+  - Microsoft Internet Explorer 6.0 *, 7.0 *, 8.0 *, 9.0 *
+* No support for multiple file selection or upload progress indication.
+
+License
+-------
+Released under the MIT license:
+http://creativecommons.org/licenses/MIT/
+
+Source Code & Download
+----------------------
+https://github.com/blueimp/jQuery-File-Upload
+
+Documentation
+-------------
+https://github.com/blueimp/jQuery-File-Upload/wiki
---
+++ jquery.fileupload-ui.js
@@ -0,0 +1,226 @@
+/*
+ * jQuery File Upload User Interface Plugin 2.1
+ *
+ * Copyright 2010, Sebastian Tschan, AQUANTUM
+ * Licensed under the MIT license:
+ * http://creativecommons.org/licenses/MIT/
+ *
+ * https://blueimp.net
+ * http://www.aquantum.de
+ */
+
+/*jslint browser: true */
+/*global jQuery */
+
+(function ($) {
+
+    var UploadHandler = function (dropZone, options) {
+        if (!options.uploadTable) {
+            $.error('jQuery.fileUploadUI requires option uploadTable: $(uploadTable)');
+        }
+        if (!options.downloadTable) {
+            $.error('jQuery.fileUploadUI requires option downloadTable: $(downloadTable)');
+        }
+        if (typeof options.buildUploadRow !== 'function') {
+            $.error('jQuery.fileUploadUI requires option buildUploadRow: function (files, index) {return $(row)}');
+        }
+        if (typeof options.buildDownloadRow !== 'function') {
+            $.error('jQuery.fileUploadUI requires option buildDownloadRow: function (json) {return $(row)}');
+        }
+        
+        var uploadHandler = this,
+            dragLeaveTimeout,
+            isDropZoneEnlarged;
+        
+        this.progressSelector = '.file_upload_progress div';
+        this.cancelSelector = '.file_upload_cancel div';
+        this.cssClassSmall = 'file_upload_small';
+        this.cssClassLarge = 'file_upload_large';
+        this.cssClassHighlight = 'file_upload_highlight';
+        this.dropEffect = 'highlight';
+
+        this.onAbort = this.removeUploadRow = function (event, files, index, xhr, settings) {
+            if (settings.uploadRow) {
+                settings.uploadRow.fadeOut(function () {
+                    $(this).remove();
+                });
+            }
+        };
+        
+        this.cancelUpload = function (files, index, xhr, settings) {
+            if (xhr && xhr.readyState > 1) {
+                xhr.abort();
+            } else {
+                // javascript:false as iframe src prevents warning popups on HTTPS in IE6
+                // concat is used here to prevent the "Script URL" JSLint error:
+                dropZone.find('iframe').attr('src', 'javascript'.concat(':false;'));
+                uploadHandler.onAbort(null, files, index, xhr, settings);
+            }
+        };
+        
+        this.initProgressBar = function (node, value) {
+            if (typeof node.progressbar === 'function') {
+                return node.progressbar({
+                    value: value
+                });
+            } else {
+                var progressbar = $('<progress value="' + value + '" max="100"/>').appendTo(node);
+                progressbar.progressbar = function (key, value) {
+                    progressbar.attr('value', value);
+                };
+                return progressbar;
+            }
+        };
+        
+        this.init = function (files, index, xhr, callBack, settings) {
+            var uploadRow = uploadHandler.buildUploadRow(files, index),
+                extraSettings = {uploadRow: uploadRow};
+            if (uploadRow) {
+                extraSettings.progressbar = uploadHandler.initProgressBar(
+                    uploadRow.find(uploadHandler.progressSelector),
+                    (xhr ? 0 : 100)
+                );
+                uploadRow.find(uploadHandler.cancelSelector).click(function () {
+                    uploadHandler.cancelUpload(files, index, xhr, extraSettings);
+                });
+                uploadRow.appendTo(uploadHandler.uploadTable).fadeIn();
+            }
+            if (typeof uploadHandler.initCallBack === 'function') {
+                uploadHandler.initCallBack(files, index, xhr, function (callBackSettings) {
+                    callBack($.extend(extraSettings, callBackSettings));
+                }, $.extend({}, settings, extraSettings));
+            } else {
+                callBack(extraSettings);
+            }
+        };
+        
+        this.onProgress = function (event, files, index, xhr, settings) {
+            if (settings.progressbar) {
+                settings.progressbar.progressbar(
+                    'value',
+                    parseInt(event.loaded / event.total * 100, 10)
+                );
+            }
+        };
+        
+        this.onLoad = function (event, files, index, xhr, settings) {
+            uploadHandler.removeUploadRow(event, files, index, xhr, settings);
+            var json, downloadRow;
+            try {
+                if (xhr) {
+                    json = $.parseJSON(xhr.responseText);
+                } else {
+                    json = $.parseJSON($(event.target).contents().text());
+                }
+                if (json) {
+                    downloadRow = uploadHandler.buildDownloadRow(json);
+                    if (downloadRow) {
+                        downloadRow.appendTo(uploadHandler.downloadTable).fadeIn();
+                    }
+                }
+                if (typeof uploadHandler.onComplete === 'function') {
+                    uploadHandler.onComplete(
+                        event, files, index, xhr, $.extend({}, settings, {downloadRow: downloadRow, response: json})
+                    );
+                }
+            } catch (e) {
+                if (typeof uploadHandler.onError === 'function') {
+                    uploadHandler.onError(
+                        e, files, index, xhr, $.extend({}, settings, {originalEvent: event})
+                    );
+                } else {
+                    throw e;
+                }
+            }
+        };
+
+        this.dropZoneEnlarge = function () {
+            if (!isDropZoneEnlarged) {
+                if (typeof dropZone.switchClass === 'function') {
+                    dropZone.switchClass(
+                        uploadHandler.cssClassSmall,
+                        uploadHandler.cssClassLarge
+                    );
+                } else {
+                    dropZone.addClass(uploadHandler.cssClassLarge);
+                    dropZone.removeClass(uploadHandler.cssClassSmall);
+                }
+                isDropZoneEnlarged = true;
+            }
+        };
+        
+        this.dropZoneReduce = function () {
+            if (typeof dropZone.switchClass === 'function') {
+                dropZone.switchClass(
+                    uploadHandler.cssClassLarge,
+                    uploadHandler.cssClassSmall
+                );
+            } else {
+                dropZone.addClass(uploadHandler.cssClassSmall);
+                dropZone.removeClass(uploadHandler.cssClassLarge);
+            }
+            isDropZoneEnlarged = false;
+        };
+
+        this.onDocumentDragEnter = function (event) {
+            setTimeout(function () {
+                if (dragLeaveTimeout) {
+                    clearTimeout(dragLeaveTimeout);
+                }
+            }, 50);
+            uploadHandler.dropZoneEnlarge();
+        };
+        
+        this.onDocumentDragLeave = function (event) {
+            if (dragLeaveTimeout) {
+                clearTimeout(dragLeaveTimeout);
+            }
+            dragLeaveTimeout = setTimeout(function () {
+                uploadHandler.dropZoneReduce();
+            }, 100);
+        };
+        
+        this.onDragEnter = this.onDragLeave = function (event) {
+            dropZone.toggleClass(uploadHandler.cssClassHighlight);
+        };
+        
+        this.onDrop = function (event) {
+            if (typeof dropZone.effect === 'function') {
+                dropZone.effect(uploadHandler.dropEffect, function () {
+                    dropZone.removeClass(uploadHandler.cssClassHighlight);
+                    uploadHandler.dropZoneReduce();
+                });
+            } else {
+                dropZone.removeClass(uploadHandler.cssClassHighlight);
+                uploadHandler.dropZoneReduce();
+            }
+        };
+
+        $.extend(this, options);
+    },
+
+    methods = {
+        init : function (options) {
+            return this.each(function () {
+                $(this).fileUpload(new UploadHandler($(this), options));
+            });
+        },
+                
+        destroy : function (namespace) {
+            return this.each(function () {
+                $(this).fileUpload('destroy', namespace);
+            });
+        }
+    };
+    
+    $.fn.fileUploadUI = function (method) {
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || ! method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Method ' + method + ' does not exist on jQuery.fileUploadUI');
+        }
+    };
+    
+}(jQuery));
