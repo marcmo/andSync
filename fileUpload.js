4d37456461264fd5e9bcb9710e9f5fee651bde5b
omueller <omueller@lear.com>
2011-01-18 16:36:21 +0100

    new way to display webpage

--- fileUpload.js
+++ fileUpload.js
@@ -10,29 +10,16 @@ var fs = require('fs'),
     jquery = require("jquery"),
 	syncUtil = require("syncUtil");
 
-
-
-
 global.puts = futil.puts;
 global.p = function() {
   futil.error(futil.inspect.apply(null, arguments));
 };
 global.PORT = 12345;
 global.UPLOADDIR = path.join(__dirname, 'uploadDir');
+global.MP3DIR = path.join(__dirname, 'mp3Folder');
 
-path.exists(UPLOADDIR, function (exists) {
-	puts(exists ? UPLOADDIR + " is already there" : "creating... ");
-	if (exists)
-	{
-		startServer();
-	}
-	else
-	{
-		fs.mkdir(UPLOADDIR,777,function(e){
-			puts(UPLOADDIR + " dir created"); 
-			startServer();
-		});
-	}
+syncUtil.ensureDirectory(UPLOADDIR, function() {
+	syncUtil.ensureDirectory(MP3DIR, startServer);
 });
 
 function serve_static_file(uri, res) {
@@ -68,7 +55,8 @@ function startServer(){
 			console.log("was an upload");
 			var form = new formidable.IncomingForm(),
 			files = [],
-			fields = [];
+			fields = [],
+			currentFile;
 
 			form.uploadDir = UPLOADDIR;
 			form.keepExtensions = true;
@@ -76,22 +64,28 @@ function startServer(){
 			form.on('field', function(field, value) {
 				p([field, value]);
 				fields.push([field, value]);
-				console.log("expected bytes:" + form.bytesExpected);
-				console.log("received bytes:" + form.bytesReceived);
+				console.log("field...expected bytes:" + form.bytesExpected);
+				console.log("field...received bytes:" + form.bytesReceived);
 			}).on('file', function(field, file) {
 				p([field, file]);
 				console.log(util.inspect(file));
 				files.push([field, file]);
-				console.log("expected bytes:" + form.bytesExpected);
-				console.log("received bytes:" + form.bytesReceived);
+				currentFile = file;
+				console.log("currentFile: " + currentFile);
+				console.log("on...pushed: field:" + field + ",file:" + file);
+				console.log("on...expected bytes:" + form.bytesExpected);
+				console.log("on...received bytes:" + form.bytesReceived);
 			}).on('end', function() {
 				puts('-> upload done');
 				res.writeHead(200, {'content-type': 'text/plain'});
 				res.write('received fields:\n\n '+util.inspect(fields));
 				res.write('\n\n');
 				res.end('received files:\n\n '+util.inspect(files));
-				console.log("expected bytes:" + form.bytesExpected);
-				console.log("received bytes:" + form.bytesReceived);
+				console.log("end...expected bytes:" + form.bytesExpected);
+				console.log("end...received bytes:" + form.bytesReceived);
+				console.log("currentFile: " + currentFile.filename);
+				console.log("currentFile: " + currentFile.path);
+				fs.rename(currentFile.path,path.join(MP3DIR,currentFile.filename));
 			});
 			form.parse(req);
 		} else if(req.url === "/stream") {  
@@ -130,21 +124,23 @@ var mp3_list_emitter = new events.EventEmitter();
 function get_mp3_list(mp3List) {
 	console.log("get_mp3_list() function..." + mp3List);
 	var mp3s = jquery.map(mp3List, function(v){
-		return {text:path.basename(v)};	
+		return {
+			name:path.basename(v),
+			modified:fs.statSync(v).mtime
+		};	
 	});
 	console.log("mp3s:" + mp3s + "---" + JSON.stringify(mp3s));
 
 	mp3_list_emitter.emit("mp3s", mp3s);
 }
 
-setInterval(update_mp3_list, 5000);
 function update_mp3_list() {
-	console.log("updateing list...");
-	var dir = path.join(process.cwd(), "mp3Folder");  
-	console.log("updateing list 2...");
-	syncUtil.flatten(dir, function(dirList) {
+	console.log(util.inspect(process.memoryUsage()));
+	syncUtil.flatten(MP3DIR, function(dirList) {
 		get_mp3_list(dirList);
 	});
 }
+
+setInterval(update_mp3_list, 5000);
 
 
