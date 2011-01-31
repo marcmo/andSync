763554255520a096a0db8ed9f027624b32c62ec0
oliver <oliver.mueller@gmail.com>
2011-01-31 22:02:00 +0100

    handling of /user/content /user/sha1 /user/upload

--- fileUpload.js
+++ fileUpload.js
@@ -21,6 +21,7 @@ global.UPLOADDIR = path.join(__dirname, 'uploadDir');
 global.MP3DIR = path.join(__dirname, 'mp3Folder');
 global.USERDIR = path.join(__dirname, 'users');
 global.USERS = [];
+global.mp3Lists = {};
 
 syncUtil.ensureDirectory(UPLOADDIR, function() {
   syncUtil.ensureDirectory(MP3DIR, function() {
@@ -30,12 +31,14 @@ syncUtil.ensureDirectory(UPLOADDIR, function() {
         jquery.map(paths, function(v,i){
           var s = fs.statSync(v);
           if (s.isDirectory()) {
-            console.log("is a user:" + s);
             USERS.push(path.basename(v));
           } 
         });
         console.log("known users: " + USERS);
-        startServer();
+
+        syncUtil.asyncMap(USERS,
+            function(u,cb){ updateMp3List(u,cb); },
+            startServer); // done callback
       });
     });
   });
@@ -68,80 +71,10 @@ function serveStaticFile(uri, res) {
 }
 
 function startServer(){
-  updateMp3List();
   var server = http.createServer(function(req, res) {
     console.log("server: req:" + req.url);
     var contentRegex = /\/content\/(.*\.\w*)/i;
-    if (req.url == '/upload') {
-      console.log("was an upload");
-      var form = new formidable.IncomingForm(),
-      files = [],
-      fields = [],
-      currentFile;
-
-      form.uploadDir = UPLOADDIR;
-      form.keepExtensions = true;
-
-      form.on('field', function(field, value) {
-        p([field, value]);
-        fields.push([field, value]);
-        console.log("field...expected bytes:" + form.bytesExpected + "received:" + form.bytesReceived);
-      }).on('file', function(field, file) {
-        p([field, file]);
-        console.log(util.inspect(file));
-        console.log(file.length);
-        files.push([field, file]);
-        currentFile = file;
-        console.log("on...pushed: field:" + field + ",file:" + file);
-        var x = crypto.createHash('sha1').update(file).digest('hex');
-        console.log("hash:" + x);
-      }).on('end', function() {
-        puts('-> upload done');
-        res.writeHead(200, {'content-type': 'text/plain'});
-        var responseObject = [];
-		jquery.map(files, function(f){
-			responseObject.push({name:f[1].filename,size:f[1].length});
-		});
-        res.write(JSON.stringify(responseObject));
-        res.end();
-        console.log("end...expected " + form.bytesExpected + " bytes, received " + form.bytesReceived + " bytes.");
-        fs.rename(currentFile.path,
-          path.join(MP3DIR,currentFile.filename),
-          updateMp3List);
-      });
-      form.parse(req);
-    } else if(req.url === "/users") {  
-      res.writeHead(200, { "Content-Type" : "text/plain" });  
-      res.write(JSON.stringify(USERS));  
-      res.end();  
-    } else if(req.url === "/content") {  
-		res.writeHead(200, { "Content-Type" : "text/plain" });  
-		res.write(JSON.stringify(myMp3List));  
-		res.end();  
-    } else if(req.url === "/sha1") {  
-      console.log("sending back sha1:" + mp3CheckSum);
-      res.writeHead(200, { "Content-Type" : "text/plain" });  
-      res.write("" + mp3CheckSum);
-      res.end();  
-    } else if(req.url === "/clear") {  
-      res.writeHead(200, { "Content-Type" : "text/plain" });  
-      fs.readdir(MP3DIR, function(err,files){
-          var deleteCount = files.length;
-          jquery.map(files, function(file){
-            if (err) {throw err;}
-            fs.unlink(path.join(MP3DIR,file), function (err) {
-                if (err) {throw err;}
-                console.log('successfully deleted ' + file + ', ' + deleteCount + ' to go...');
-                deleteCount--;
-                if(deleteCount === 0){
-                  updateMp3List();
-                  res.write(JSON.stringify(myMp3List));  
-                  res.end();
-                }
-            });
-          });
-      });
-    } else if (contentRegex.test(req.url)) {
+    if (contentRegex.test(req.url)) {
       console.log("content url was:" + req.url);
       var matchedMp3 = contentRegex.exec(req.url)[1];
       serveStaticFile(path.join("mp3Folder",unescape(matchedMp3)),res);
@@ -149,35 +82,8 @@ function startServer(){
       console.log("was a script,url:"+req.url);
       var uri = url.parse(req.url).pathname;  
       serveStaticFile(uri,res);
-    // } else if (req.url.match("^\/user")) {
-
-    } else if (req.url == '/user/new') {
-      var form = new formidable.IncomingForm(),
-      fields = [];
-      form
-      .on('error', function(err) {
-        res.writeHead(200, {'content-type': 'text/plain'});
-        res.end('error:\n\n'+util.inspect(err));
-      })
-      .on('field', function(field, value) {
-        p([field, value]);
-        console.log('got new field:' + field + ',value:' + value);
-        fields.push(value);
-        USERS.push(value);
-      })
-      .on('end', function() {
-        var newFolders = jquery.map(fields, function(v){ return path.join(USERDIR,v); });
-        console.log("new users:" + fields);
-        syncUtil.asyncMap(newFolders,
-            function(x,cb){fs.mkdir(x,0777,cb);}, // partial function application
-            function(x){
-              puts('-> post done');
-              res.writeHead(200, {'content-type': 'text/plain'});
-              res.end('received fields:\n\n '+util.inspect(fields));
-              console.log('done!'+x);
-            }); // done callback
-      });
-      form.parse(req);
+    } else if (req.url.match("^\/user\/")) {
+      handleUserOperation(req,res);
     } else {
       res.writeHead(404, {'content-type': 'text/plain'});
       res.end('404');
@@ -187,30 +93,165 @@ function startServer(){
   util.puts('listening on http://localhost:'+PORT+'/');
 }
 
-var myMp3List = [];
-function rebuildMp3List(mp3List) {
-  console.log("rebuildMp3List() function..." + mp3List);
-  myMp3List = jquery.map(mp3List, function(v){
+function handleUserOperation(req,res){
+  if (req.url == '/user/new') {
+    var form = new formidable.IncomingForm(),
+    fields = [];
+    form
+    .on('error', function(err) {
+      res.writeHead(200, {'content-type': 'text/plain'});
+      res.end('error:\n\n'+util.inspect(err));
+    })
+    .on('field', function(field, value) {
+      p([field, value]);
+      console.log('got new field:' + field + ',value:' + value);
+      fields.push(value);
+      USERS.push(value);
+    })
+    .on('end', function() {
+      var newFolders = jquery.map(fields, function(v){ return path.join(USERDIR,v); });
+      console.log("new users:" + fields);
+      syncUtil.asyncMap(newFolders,
+          function(x,cb){fs.mkdir(x,0777,cb);}, // partial function application
+          function(x){
+            puts('-> post done');
+            res.writeHead(200, {'content-type': 'text/plain'});
+            res.end('received fields:\n\n '+util.inspect(fields));
+            console.log('done!'+x);
+          }); // done callback
+    });
+    form.parse(req);
+  } else if (req.url == '/user/list'){
+    res.writeHead(200, { "Content-Type" : "text/plain" });  
+    res.write(JSON.stringify(USERS));  
+    res.end();  
+  } else {
+    var userRegex = /\/user\/([\w\s\d]*)[\/]{0,1}(.*)/i;
+    console.log("user url was:" + req.url);
+    var matchedUser = userRegex.exec(req.url)[1];
+    var userDir = path.join(USERDIR,matchedUser);
+    path.exists(userDir, function (exists) {
+      console.log('user action:' + matchedUser);
+      if (exists){
+        var matchedOp = userRegex.exec(req.url)[2];
+        singleUserOp(req,res,matchedUser,matchedOp);
+      } else {
+        res.writeHead(404, {'content-type': 'text/plain'});
+        res.end('404');
+      }
+    });
+  }
+}
+
+function singleUserOp(req,res,user,op){
+  if (op === 'content') {
+    res.writeHead(200, { "Content-Type" : "text/plain" });  
+    console.log(mp3Lists);
+    console.log('trying to read user:' + user + ',mp3List=' + mp3Lists);
+    res.write(JSON.stringify(mp3Lists[user].music));  
+    res.end();  
+  } else if (op === "sha1") {  
+    console.log("sending back sha1:" + mp3Lists[user].sha1);
+    res.writeHead(200, { "Content-Type" : "text/plain" });  
+    res.write("" + mp3Lists[user].sha1);
+    res.end();  
+  } else if (op === "clear") {  
+    res.writeHead(200, { "Content-Type" : "text/plain" });  
+    var userDir = path.join(USERDIR,matchedUser);
+    fs.readdir(userDir, function(err,files){
+        var deleteCount = files.length;
+        jquery.map(files, function(file) {
+            if (err) {throw err;}
+            fs.unlink(path.join(userDir,file), function (err) {
+                if (err) {throw err;}
+                console.log('successfully deleted ' + file + ', ' + deleteCount + ' to go...');
+                deleteCount--;
+                if (deleteCount === 0) {
+                  updateMp3List(user,function(){
+                      res.write(JSON.stringify(mp3Lists[user].music));  
+                      res.end();
+                  });
+                }
+            });
+        });
+    });
+  } else if (op === 'upload') {
+    handleUpload(req,res,user);
+  } else {
+    res.writeHead(404, {'content-type': 'text/plain'});
+    res.end('404');
+  }
+}
+
+function handleUpload(req,res,user){
+  console.log("was an upload for:" + user);
+  var form = new formidable.IncomingForm(),
+  files = [],
+  fields = [],
+  currentFile;
+
+  form.uploadDir = UPLOADDIR;
+  form.keepExtensions = true;
+  form.on('field', function(field, value) {
+    p([field, value]);
+    fields.push([field, value]);
+    console.log("field...expected bytes:" + form.bytesExpected + "received:" + form.bytesReceived);
+  }).on('file', function(field, file) {
+    p([field, file]);
+    console.log(util.inspect(file));
+    console.log(file.length);
+    files.push([field, file]);
+    currentFile = file;
+    console.log("on...pushed: field:" + field + ",file:" + file);
+    var x = crypto.createHash('sha1').update(file).digest('hex');
+    console.log("hash:" + x);
+  }).on('end', function() {
+    puts('-> upload done');
+    res.writeHead(200, {'content-type': 'text/plain'});
+    var responseObject = [];
+    jquery.map(files, function(f){
+      responseObject.push({name:f[1].filename,size:f[1].length});
+    });
+    console.log("end...expected " + form.bytesExpected + " bytes, received " + form.bytesReceived + " bytes.");
+    fs.rename(currentFile.path,
+      path.join(MP3DIR,currentFile.filename),
+      function(){updateMp3List(user,function(){
+          res.write(JSON.stringify(responseObject));
+          res.end();
+        });
+      });
+  });
+  form.parse(req);
+}
+
+function rebuildMp3List(user, mp3List) {
+  console.log("rebuildMp3List() function for:" + user);
+  var trackList = jquery.map(mp3List, function(v){
     return {
       name:path.basename(v),
       modified:fs.statSync(v).mtime,
       size:fs.statSync(v).size
     };  
   });
+  mp3Lists[user] = {music:trackList};
 }
-var mp3CheckSum = 0;
-function updateChecksum(files){
+function updateChecksum(user,files,callback){
     cryptoHelper.calcSha1(files,function (res) {
-	  console.log('checksums: old:' + mp3CheckSum + ', new:' + res);
-      mp3CheckSum = res;
+	  // console.log('checksums: old:' + mp3Lists[user].sha1 + ', new:' + res);
+      console.log('checksums: new:' + res);
+      mp3Lists[user]['sha1'] = res;
+      console.log(mp3Lists);
+      callback();
     });
 }
 
-function updateMp3List() {
+function updateMp3List(user,callback) {
+  var userDir = path.join(USERDIR,user);
   console.log(util.inspect(process.memoryUsage()));
-  syncUtil.flatten(MP3DIR, function(dirList) {
-    rebuildMp3List(dirList);
-    updateChecksum(dirList);
+  syncUtil.flatten(userDir, function(dirList) {
+    rebuildMp3List(user, dirList);
+    console.log(mp3Lists);
+    updateChecksum(user,dirList,callback);
   });
 }
 
